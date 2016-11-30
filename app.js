fbcc5790c6072513fa5962b56e199ca64932fb5b
ForgottenSonata <forgottensonata@gmail.com>
2016-11-30 08:57:20 -0800

    tuesday work

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var productImages = [];
 function Products (productName, filePath) {
     this.productName = productName;
     this.filePath = filePath;
+    this.clickTotal = 0;
+    this.timesDisplayed = 0;
     productImages.push(this);
 }
 var bag = new Products('Star Wars Luggage', 'Images/bag.jpg');
@@ -24,35 +26,90 @@ var wineGlass = new Products('Wine Glass', 'Images/wine-glass.jpg');
 
 
 var imgRandom = function () {
+
     return Math.floor(Math.random() * productImages.length);
-}
+  };
+
 var imageAppear = function(){
     var productImageOne = document.getElementById('imageOne');
-    var img1 = imgRandom();
-    productImageOne.src = productImages[img1].filePath;
     var productImageTwo = document.getElementById('imageTwo');
-    var img2 = imgRandom();
-    while (img1 === img2) {
-        img2 = imgRandom();
-    };
-    productImageTwo.src = productImages[img2].filePath;
     var productImageThree = document.getElementById('imageThree');
-    var img3 = imgRandom();
-    while (img1 === img2 || img2 === img3 || img3 === img1) {
-        img3 = imgRandom();
-    };
+    img1 = imgRandom();
+    productImageOne.src = productImages[img1].filePath;
+    productImages[img1].timesDisplayed++;
+    img2 = imgRandom();
+    productImageTwo.src = productImages[img2].filePath;
+    productImages[img2].timesDisplayed++;
+    img3 = imgRandom();
     productImageThree.src = productImages[img3].filePath;
+    productImages[img3].timesDisplayed++;
+
+};
+
+
+
+function renderChart () {
+  var ctx = document.getElementById('my_chart');
+
+  var myChart =
+   new Chart(ctx, {
+  type: 'bar',
+    data: {
+        labels: ["bag", "banana", "bathroom", "boots", "breakfast", "bubblegum","chair","cthulu","dog-duck", "dragon", "pen", "pet-sweep", "scissors", "shark", "sweep", "tauntaun", "unicorn", "usb", "water-can", "wine-glass"],
+        datasets: [{
+            label: '# of Votes',
+            data: [5,3,6, 7, 5],
+            backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(25, 200, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 103, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(300, 25, 150, 1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 5
+       }]
+   },
+   options: {
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   }
+});
 }
-imageAppear();
+
+
 
 var firstClick = 0;
 var secondClick = 0;
 var thirdClick = 0;
 
-function handleClick(){
+function handleClick(image){
+
     imageAppear();
 }
 
-imageOne.addEventListener('click', handleClick);
-imageTwo.addEventListener('click', handleClick);
-imageThree.addEventListener('click', handleClick);
+imageOne.addEventListener('click', function(){
+  handleClick(productImages[img1]);
+});
+imageTwo.addEventListener('click', function(){
+  handleClick(productImages[img2]);
+});
+imageThree.addEventListener('click', function(){
+handleClick(productImages[img3]);
+});
+
+renderChart();
+imageAppear();
